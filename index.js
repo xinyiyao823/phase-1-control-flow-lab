3cc3e5c81cf94068caeab04348d60663680d996d
+ <+>
2021-08-31 08:03:29 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+let greeting;
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400) {
+    return greeting = 'This one is on me!';
+  } else if (feet > 2000 && feet < 2499) {
+      return greeting = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return greeting = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return greeting = 'Ok, sounds good.';
+  } else {
+    return greeting = 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous') {
+    return greeting = 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+      return greeting = 'Thank you.';
+  } else {
+      return greeting = 'Bye.';
+  }
 }
